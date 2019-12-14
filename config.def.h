78a54dc623124f67cc12337750a89af5d0a26d86
daniel eliad <daniel.eliad32@gmail.com>
2019-12-14 15:53:12 +0200

    increased bar padding

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int tbpad              = 10;       /* sum of top and bottom padding for text */
+static const int tbpad              = 12;       /* sum of top and bottom padding for text */
 static const char *fonts[]          = { "Inconsolate for Powerline:pixelsize=16:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Inconsolate for Powerline:pixelsize=16:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
